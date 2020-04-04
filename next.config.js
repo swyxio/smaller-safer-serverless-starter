c7c6aafa20a1608631670da17f3fc2a8403ef548
swyx <shawnthe1@gmail.com>
2020-04-04 12:52:28 +0800

    polish up and add typescript

--- next.config.js
+++ next.config.js
@@ -1,8 +1,43 @@
 module.exports = {
-  webpack: (config) => {
-    config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
+  // https://github.com/developit/nextjs-preact-demo/pull/6/files
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true,
+  },
+  webpack(config, { dev, isServer }) {
+    config = preactModifications({ config, dev, isServer });
+    return config;
+  },
 };
+
+// https://github.com/developit/nextjs-preact-demo/
+function preactModifications({ config, dev, isServer }) {
+  const splitChunks = config.optimization && config.optimization.splitChunks;
+  if (splitChunks) {
+    const cacheGroups = splitChunks.cacheGroups;
+    const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+    if (cacheGroups.framework) {
+      cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+        test: preactModules,
+      });
+      cacheGroups.commons.name = "framework";
+    } else {
+      cacheGroups.preact = {
+        name: "commons",
+        chunks: "all",
+        test: preactModules,
+      };
+    }
+  }
+
+  // inject Preact DevTools
+  if (dev && !isServer) {
+    const entry = config.entry;
+    config.entry = () =>
+      entry().then((entries) => {
+        entries["main.js"] = ["preact/debug"].concat(entries["main.js"] || []);
+        return entries;
+      });
+  }
+  return config;
+}
--- postcss.config.js
+++ postcss.config.js
@@ -4,18 +4,22 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
+    // // https://purgecss.com/configuration.html#options
+    // // you may need to whitelist some things with other modules
+    // css: [],
+    // whitelistPatternsChildren: [],
     content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
     ],
-    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
-  }
+    defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+  },
 ];
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
