46c8186ee778da40afe49edc5dd1e3ddf8278300
swyx <shawnthe1@gmail.com>
2020-04-04 16:39:43 +0800

    fixed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  // // https://github.com/developit/nextjs-preact-demo/pull/6/files
-  // experimental: {
-  //   modern: true, // https://github.com/zeit/next.js/issues/7563
-  //   polyfillsOptimization: true, // https://github.com/zeit/next.js/pull/10212
-  // },
+  // https://github.com/developit/nextjs-preact-demo/pull/6/files
+  experimental: {
+    modern: true, // https://github.com/zeit/next.js/issues/7563
+    polyfillsOptimization: true, // https://github.com/zeit/next.js/pull/10212
+  },
   webpack(config, { dev, isServer }) {
     config = preactModifications({ config, dev, isServer });
     return config;
   },
-  // devIndicators: {
-  //   autoPrerender: false,
-  // },
-  // target: "serverless", // https://zeit.co/docs/v2/serverless-functions/env-and-secrets?query=serverless#
+  devIndicators: {
+    autoPrerender: false,
+  },
+  target: "serverless", // https://zeit.co/docs/v2/serverless-functions/env-and-secrets?query=serverless#
 };
 
 // https://github.com/developit/nextjs-preact-demo/
