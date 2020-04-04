44690d96b546fc6a1cb99cb28963f36a83bbd662
swyx <shawnthe1@gmail.com>
2020-04-04 16:39:34 +0800

    fixed

--- next.config.js
+++ next.config.js
@@ -16,23 +16,23 @@ module.exports = {
 
 // https://github.com/developit/nextjs-preact-demo/
 function preactModifications({ config, dev, isServer }) {
-  const splitChunks = config.optimization && config.optimization.splitChunks;
-  if (splitChunks) {
-    const cacheGroups = splitChunks.cacheGroups;
-    const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-    if (cacheGroups.framework) {
-      cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-        test: preactModules,
-      });
-      cacheGroups.commons.name = "framework";
-    } else {
-      cacheGroups.preact = {
-        name: "commons",
-        chunks: "all",
-        test: preactModules,
-      };
-    }
-  }
+  // const splitChunks = config.optimization && config.optimization.splitChunks;
+  // if (splitChunks) {
+  //   const cacheGroups = splitChunks.cacheGroups;
+  //   const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+  //   if (cacheGroups.framework) {
+  //     cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+  //       test: preactModules,
+  //     });
+  //     cacheGroups.commons.name = "framework";
+  //   } else {
+  //     cacheGroups.preact = {
+  //       name: "commons",
+  //       chunks: "all",
+  //       test: preactModules,
+  //     };
+  //   }
+  // }
 
   // inject Preact DevTools
   if (dev && !isServer) {
